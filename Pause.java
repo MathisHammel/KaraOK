ca95fbf768d2e6f07b2d906bf3514ba16d619e12
j0bby <mathieuvirsolvy@gmail.com>
2015-05-18 15:36:20 +0200

    added GameWindow.java and Pause.java

---
+++ Pause.java
@@ -0,0 +1,14 @@
+import java.awt.event.KeyEvent;
+
+public class Pause {
+    
+    public static void Update()
+     {
+      // instructions en fonction de la touche préssée.
+       
+     }
+     public static void Draw()
+     {
+      // dessiner une image avec les instructions
+     }
+}
