1b70e3591feadcb87322beff031682462104c433
kara71 <mathis.hammel@gmail.com>
2015-05-21 00:39:42 +0200

    Update Button for active buttons
    Button constructor now has 2 arguments to use when hovered

--- Button.java
+++ Button.java
@@ -1,28 +1,64 @@
 import java.awt.Graphics;
 import java.awt.Image;
-import javax.swing.ImageIcon;
-import javax.swing.JFrame;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import javax.swing.JButton;
+import javax.swing.ImageIcon;
+  
+public class Button extends JButton implements MouseListener{
+
+	private static final long serialVersionUID = 1L;
+	
+  private Image imgOff;
+  private Image imgOn;
+  private Image img;
+
+  
+
+  public Button(ImageIcon iconOff, ImageIcon iconOn){
+    super(iconOff);
+    
+    imgOff=iconOff.getImage();
+    imgOn=iconOn.getImage();
+
+  this.addMouseListener(this);
+  }
+
+  public Button(ImageIcon image) {
+      super(image);
+      imgOff= image.getImage();
+      imgOn= image.getImage();
+      img=imgOff;
+  }
+  
+  protected void paintComponent(Graphics g) {
+      super.paintComponent(g);
+      if (img == null) return;
+      g.drawImage(img, 0, 0, getWidth(), getHeight(), this);
+  }
+  
+  protected void setIcon(ImageIcon image){
+      super.setIcon(image);
+      img= image.getImage();
+  }
+  
+  public void mouseClicked(MouseEvent event) {
+                    
+  }
+
+  public void mouseEntered(MouseEvent event) {
+      img = imgOn;
+  }
+
+  public void mouseExited(MouseEvent event) {
+	  img = imgOff;
+  }
+
+  public void mousePressed(MouseEvent event) {
+
+  }
  
-public class Button extends JButton { 
-     
-    private Image img;
-     
-    //Un constructeur pour choisir plus simplement l'image
-    public Button(ImageIcon image) {
-        super(image);
-        img= image.getImage();
-    }
-     
-    //On doit redéfinir la méthode paintComponent() pour les composant swing !!! et paint() pour awt
-    @Override
-    protected void paintComponent(Graphics g) {
-        super.paintComponent(g);
-        if (img == null) return;
-        g.drawImage(img, 0, 0, getWidth(), getHeight(), this); //ici on adapte l'image à la taille du composant(getWidth(), getHeight())...
-    }
-    protected void setIcon(ImageIcon image){
-        super.setIcon(image);
-        img= image.getImage();
-    }
+  public void mouseReleased(MouseEvent event) {
+          
+  }       
 }
