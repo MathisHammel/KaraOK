bbc7ead36b2513eeb90388fc49abdeed335ba0b9
kara71 <mathis.hammel@gmail.com>
2015-06-06 19:49:00 +0200

    Commented and cleaned Autocorrelate
    Removed useless code and commented all functions

--- Autocorrelate.java
+++ Autocorrelate.java
@@ -1,11 +1,11 @@
 
 public class Autocorrelate {
-	public static double[] compute(byte[] x){ //compute autocorrelation and return array
+	public static double[] compute(byte[] x){ //compute autocorrelation and return computed array
 		int N=x.length;
 		double out[] = new double[N/2];
 		int i;
 		int j;
-		for(i=0;i<N/2;i++)
+		for(i=0;i<N/2;i++)//simple discrete signal autocorrelation
 		{
 			out[i]=0;
 			for(j=0;j<N-i;j++)
@@ -13,18 +13,18 @@ public class Autocorrelate {
 				out[i]+=x[j]*x[j+i];
 			}
 			out[i]/=(N-i);
-			//out[i]=Math.abs(out[i]);
+			
 		}
 		
 		return(out);
 	}
 	
-	public static void computeShow(byte[] x){ //compute autocorrelation and display the array
+	public static void computeShow(byte[] x){ //compute autocorrelation and display the computed array (no return, just used for debug)
 		int N=x.length;
 		double out[] = new double[N/2];
 		int i;
 		int j;
-		for(i=0;i<N/2;i++)
+		for(i=0;i<N/2;i++)//simple discrete signal autocorrelation
 		{
 			out[i]=0;
 			for(j=0;j<N-i;j++)
@@ -32,12 +32,12 @@ public class Autocorrelate {
 				out[i]+=x[j]*x[j+i];
 			}
 			out[i]/=(N-i);
-			//out[i]=Math.abs(out[i]);
+			
 			System.out.println("Decalage "+i+" : "+out[i]);
 		}
 	}
 		
-	public static double freq(byte[] x)
+	public static double freq(byte[] x) //this function is based on compute(), checks the first peak after offset 0
 		{
 			int i;
 			int freq = -1;
@@ -52,22 +52,22 @@ public class Autocorrelate {
 			int imax=0;
 			
 			i=15;
-			while(y[i]<0.6*y[0] && i<y.length-1)
+			while(y[i]<0.6*y[0] && i<y.length-1)//array iterator to wait for peak beginning
 			{
 				i++;
 			}
-			if(i==(y.length)-2)
+			if(i==(y.length)-2) //if reached end of computed array without peak (which means no correct signal), return 0
 			{
 				return(0.0);
 			}
 			imin=i;
-			while(y[i]>0.5*y[0] && i<y.length-1)
+			while(y[i]>0.5*y[0] && i<y.length-1)//second array iterator to determine peak end
 			{
 				i++;
 			}
 			imax=i;
 			max=y[imin];
-			for(i=imin;i<=imax;i++)
+			for(i=imin;i<=imax;i++)//iterator on peak to determine actual maximum
 			{
 				if(y[i]>max)
 				{
@@ -77,16 +77,8 @@ public class Autocorrelate {
 			}
 			
 			
-			freq=44100/n;
-			/*while(freq>=880)
-			{
-				freq/=2;
-			}
-			while(freq<440)
-			{
-				System.out.println(freq);
-				freq*=2;
-			}*/
+			freq=44100/n;//divide sample rate by autocorrelate period peak to get actual frequency
+			
 			return(freq);
 		
 		}
