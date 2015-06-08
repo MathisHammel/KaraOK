8ebefcc23664f2080ac692f0f772ac1dd7dff67e
j0bby <mathieuvirsolvy@gmail.com>
2015-06-08 17:19:55 +0200

    updated Score.java

--- Score.java
+++ Score.java
@@ -1,13 +1,14 @@
 public class Score {
     
     
-    public static void calculScore(int currentscore, String note, double frequency){
+    public static void calculScore(int currentscore,int scoremax, String note, double frequency){
         if(note.equals("")){
             
         }else{
         double freq= frequency-660;
         double noteFreq = FreqThread.getFreq(note)-660;
         currentscore+= 10-Math.abs((Math.abs(freq)- Math.abs(noteFreq)))/22;
+        scoremax+=10;
         }
     }
 }
